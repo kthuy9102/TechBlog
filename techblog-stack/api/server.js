fa4e39e2cf9505f52873c7d7ba0b345926d74bc7
kthuy9102 <kaypham4869@gmail.com>
2026-09-22 23:58:24 +0700

    Optimize Dockerfile, scale API and config Nginx

--- techblog-stack/api/server.js
+++ techblog-stack/api/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const os = require("os"); // Thêm dòng này để lấy thông tin container
 const app = express();
+
 // Cấu hình đọc từ biến môi trường do Compose truyền vào
 const DB_HOST = process.env.DB_HOST;   // = "db"
 const CACHE_HOST = process.env.CACHE_HOST; // = "cache"
- 
+
 app.get("/", (req, res) => {
-  res.send(`API ok. DB=${DB_HOST}, CACHE=${CACHE_HOST}`);
+  // Thêm os.hostname() vào chuỗi trả về
+  res.send(`API ok. Host=${os.hostname()}, DB=${DB_HOST}, CACHE=${CACHE_HOST}`);
 });
+
 app.listen(3000, () => console.log("API listening on 3000"));
